8e386ff53d75ab1aa0a1c4250e0fb1648acbba5d
Spaze-01 <spaze.games1@gmail.com>
2024-06-29 22:51:52 +0800

    Generate HTML with JavaScript

---
+++ main/scripts/yumdash.js
@@ -0,0 +1,62 @@
+const wcdoMenu = [{
+  logo: '/main/images/logo/wcdonalds-logo.png',
+  image: '/main/images/menu/wcdonalds/chicken/1pc-chicken-with-rice.png',
+  name: '1-pc. Chicken WcDo w/ Rice Small Meal',
+  priceCents: 11900
+}, {
+  logo: '/main/images/logo/wcdonalds-logo.png',
+  image: '/main/images/menu/wcdonalds/chicken/1pc-chicken-with-rice.png',
+  name: '1-pc. Chicken WcDo w/ Rice Small Meal',
+  priceCents: 11900
+}, {
+  logo: '/main/images/logo/wcdonalds-logo.png',
+  image: '/main/images/menu/wcdonalds/chicken/1pc-chicken-with-rice.png',
+  name: '1-pc. Chicken WcDo w/ Rice Small Meal',
+  priceCents: 11900
+}];
+
+let wcdoMenuHTML = '';
+
+wcdoMenu.forEach((menu) => {
+  wcdoMenuHTML += `
+        <div class="food-menu-container">
+          
+          <div class="restaurant-logo-container">
+            <img class="restaurant-logo" src="${menu.logo}">
+          </div>
+          
+          <div class="food-image-container">
+            <img class="food-image" src="${menu.image}">
+          </div>
+          
+          <div class="food-name limit-text-to-2-lines">
+            ${menu.name}
+          </div>
+          
+          <div class="horizontal-line"></div>
+          
+          <div class="food-price">
+            &#8369; ${(menu.priceCents / 100).toFixed(2)}
+          </div>
+          
+          <div class="food-quantity-container">
+            <button class="button-quantity">-</button>
+            <span class="food-quantity">2</span>
+            <button class="button-quantity">+</button>
+          </div>
+          
+          <div class="added-to-bag">
+            <img src="/main/images/icons/checkmark.png">
+            Added
+          </div>
+          
+          <button class="add-to-my-bag-button button-primary">
+            Add to My Bag
+          </button>
+        </div>
+  `;
+});
+
+console.log(wcdoMenuHTML);
+
+document.querySelector('.js-menu-grid').innerHTML = wcdoMenuHTML;
